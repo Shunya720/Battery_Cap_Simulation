e423d3d649d96dd45d6e2295bb3d51012cd6f766
Shunya720 <shunya-gima@o-enetech.co.jp>
2025-06-12 10:23:58 +0900

    Update annual_capacity_simulator.py

--- annual_capacity_simulator.py
+++ annual_capacity_simulator.py
@@ -1087,7 +1087,7 @@ def display_annual_results():
         def show_annual_demand_comparison(results, capacity_list, annual_demand):
         
     """年間需要比較タブの内容"""
-    st.subheader("年間需要カーブ比較")
+        st.subheader("年間需要カーブ比較")
     
     # グラフ表示期間選択
     col1, col2, col3 = st.columns(3)
